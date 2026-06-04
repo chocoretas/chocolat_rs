a4b000e1669a49ab75cfef1d71e088cd2785ce22
MilaCamila <mila.gastelum@autistici.org>
2026-06-03 22:43:44 -0700

    Latencia funcional basada en soulwave

--- src/commands/util/ping.rs
+++ src/commands/util/ping.rs
@@ -2,6 +2,21 @@ use crate::command::*;
 
 pub struct Ping;
 
+use std::time::Instant;
+
+use serenity::all::{
+    CreateMessage,
+    CreateEmbed,
+    CreateEmbedFooter,
+    EditMessage,
+};
+
+use crate::command::{
+    Command,
+    CommandInfo,
+    CommandRegistration,
+};
+
 #[async_trait::async_trait]
 impl Command for Ping {
     fn info(&self) -> CommandInfo {
@@ -15,9 +30,42 @@ impl Command for Ping {
         &self,
         ctx: &serenity::all::Context,
         msg: &serenity::all::Message,
-        args: Vec<String>,
+        _args: Vec<String>,//Since no lo usamos
     ) -> serenity::Result<()> {
-        msg.reply(ctx, "Pong!").await?;
+        //Logica del comando
+        let start = Instant::now();
+
+        let mut calculating = msg
+            .channel_id
+            .send_message(
+                &ctx.http,
+                CreateMessage::new()
+                    .content("⏳ Calculando latencia..."),
+            )
+            .await?;
+
+        let latency = start.elapsed().as_millis();
+
+        let embed = CreateEmbed::new()
+            .title("🏓 Pong!")
+            .description(format!(
+                "📡 Latencia: `{latency}` ms"
+            ))
+            .footer(
+                CreateEmbedFooter::new(
+                    "Tiempo de respuesta medido."
+                )
+            );
+
+        calculating
+            .edit(
+                &ctx.http,
+                EditMessage::new()
+                    .content("")
+                    .embed(embed),
+            )
+            .await?;
+
         Ok(())
     }
 }
